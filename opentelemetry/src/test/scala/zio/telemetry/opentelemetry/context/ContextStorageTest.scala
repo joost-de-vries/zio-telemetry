a24b7035b2405b32af5f76a28d9c56cb0297a5df
Joost de Vries <joost@ziener.org>
2025-01-07 15:12:52 +0100

    test ContextStorage.native

---
+++ opentelemetry/src/test/scala/zio/telemetry/opentelemetry/context/ContextStorageTest.scala
@@ -0,0 +1,89 @@
+package zio.telemetry.opentelemetry.context
+
+import io.opentelemetry.context.{Context, ContextKey}
+import zio.{Scope, ZIO}
+import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue}
+
+object ContextStorageTest extends ZIOSpecDefault {
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite("zio opentelemetry")(
+      suite("ContextStorage.native")(
+        test("set should restore context after scope") {
+          for {
+            contextStorage <- ZIO.service[ContextStorage]
+            before          = Context.current()
+            updated         = before.`with`(key, "local value")
+            withinScope    <- ZIO.scoped[Any](contextStorage.set(updated) *> contextStorage.get)
+            after           = Context.current()
+          } yield assertTrue(
+            withinScope.get(key) == "local value",
+            after.get(key) == null,
+            after == before
+          )
+        },
+        test("getAndSet should restore context after scope") {
+          for {
+            contextStorage            <- ZIO.service[ContextStorage]
+            before                     = Context.current()
+            updated                    = before.`with`(key, "local value")
+            (likeBefore, withinScope) <- ZIO.scoped[Any](contextStorage.getAndSet(updated) <*> contextStorage.get)
+            after                      = Context.current()
+          } yield assertTrue(
+            likeBefore == before,
+            withinScope.get(key) == "local value",
+            after.get(key) == null,
+            after == before
+          )
+        },
+        test("updateAndGet should restore context after scope") {
+          for {
+            contextStorage               <- ZIO.service[ContextStorage]
+            before                        = Context.current()
+            (updated, afterUpdateAndGet) <-
+              ZIO.scoped[Any] {
+                contextStorage.updateAndGet(c => c.`with`(key, "local value")) <*> contextStorage.get
+              }
+            after                         = Context.current()
+          } yield assertTrue(
+            updated.get(key) == "local value",
+            updated == afterUpdateAndGet,
+            after.get(key) == null,
+            after == before
+          )
+        },
+        test("locally should restore context after scope") {
+          for {
+            contextStorage <- ZIO.service[ContextStorage]
+            before          = Context.current()
+            updated         = before.`with`(key, "local value")
+            value          <- contextStorage.locally(updated) {
+                                contextStorage.get.map(_.get(key))
+                              }
+            after           = Context.current()
+          } yield assertTrue(
+            value == "local value",
+            after.get(key) == null,
+            after == before
+          )
+        },
+        test("locallyScoped should restore context after scope") {
+          for {
+            contextStorage <- ZIO.service[ContextStorage]
+            before          = Context.current()
+            updated         = before.`with`(key, "local value")
+            withinScope    <- ZIO.scoped[Any] {
+                                contextStorage.locallyScoped(updated) *> contextStorage.get
+                              }
+            after           = Context.current()
+          } yield assertTrue(
+            after.get(key) == null,
+            withinScope.get(key) == "local value",
+            after == before
+          )
+        }
+      ).provide(ContextStorage.native)
+    )
+
+  private val key = ContextKey.named[String]("local-only")
+
+}
